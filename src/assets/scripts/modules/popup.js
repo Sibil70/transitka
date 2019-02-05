3cc245da08023a2db9b311a89d0ae129ea25ab19
Alexander Varnaev <alexandervarnaev@gmail.com>
2019-02-05 16:52:22 +0300

    delete some unneccecary files

--- src/assets/scripts/modules/popup.js
+++ src/assets/scripts/modules/popup.js
@@ -1,19 +1,18 @@
 $(function() {
 
     var deposit = $("#popup");
-    // console.log(deposit);
-    // deposit.hide();
+    deposit.hide();
 
-    // window.allowpop = true;
-    // document.addEventListener("mouseout", function(e) {
-    //     if (e.clientY < 0 && window.allowpop) {
-    //         window.allowpop = false;
-    //         deposit.fadeIn(400);
-    //         setTimeout(function() {
-    //             window.allowpop = true;
-    //         }, 5 * 30 * 1000);
-    //     }
-    // });
+    window.allowpop = true;
+    document.addEventListener("mouseout", function(e) {
+        if (e.clientY < 0 && window.allowpop) {
+            window.allowpop = false;
+            deposit.fadeIn(400);
+            setTimeout(function() {
+                window.allowpop = true;
+            }, 5 * 30 * 1000);
+        }
+    });
 
     $(".js-close-popup").on("click", function(e) {
         e.preventDefault();
--- src/assets/styles/variable.js
+++ src/assets/styles/variable.js
@@ -1,10 +1,13 @@
 module.exports = {
-    "text-color": "#7A8F99",
+    "btn": "#20A86B",
+    "btn_red": "#f31138",
+    "text-color": "#d8e1e6",
+    "placeholder": "#7a8f99",
     "border": "#1C945D",
     "background": "#0b172a",
-    "btn": "#1C945D",
-    "input_bg": "#212d3e",
+    "input_bg": "#f5f8fa19", // 10% opacity
     "input_border": "#20A86B",
     "input_border-hover": "#20A86B88",
-    "error": "#E15433"
+    "error": "#E15433",
+    "white": "#ffffff"
 };
