c37ac9a816f482f81fa68670ec0a3af690c0713c
Alexander Varnaev <alexandervarnaev@gmail.com>
2019-02-05 12:05:25 +0300

    popup

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,3 +1,2 @@
-import "./modules/validation.js";
 import "./modules/popup.js"
 import "./modules/placeholder.js"
--- src/assets/scripts/modules/popup.js
+++ src/assets/scripts/modules/popup.js
@@ -1,8 +1,24 @@
+$(function() {
 
-    var popup = document.getElementById('popup')
-    var closePopup = document.querySelectorAll('.popup__selector');
+    var deposit = $("#popup");
+    // console.log(deposit);
+    // deposit.hide();
 
-    $(closePopup).click(function(event) {
-        $(popup).fadeOut(500);
-       });
+    // window.allowpop = true;
+    // document.addEventListener("mouseout", function(e) {
+    //     if (e.clientY < 0 && window.allowpop) {
+    //         window.allowpop = false;
+    //         deposit.fadeIn(400);
+    //         setTimeout(function() {
+    //             window.allowpop = true;
+    //         }, 5 * 30 * 1000);
+    //     }
+    // });
 
+    $(".js-close-popup").on("click", function(e) {
+        e.preventDefault();
+
+        deposit.fadeOut(200);
+        return false;
+    });
+});
